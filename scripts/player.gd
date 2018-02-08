f24d73cd2377f23d6de82bc3fa3982b1ee7c4af4
Jan Mrazek <mrazeja7@ksu.edu>
2018-02-08 15:47:07 -0600

    change camera keymap

--- scripts/player.gd
+++ scripts/player.gd
@@ -78,7 +78,7 @@ func _input(ie):
 	if ie is InputEventKey and Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 		pass
-	if Input.is_key_pressed(KEY_N):
+	if Input.is_key_pressed(KEY_C):
 		rotate_camera()
 		pass
 	
